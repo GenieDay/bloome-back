218a10f519f6f2cdf8f710b82622cfbcfa9a9296
yejinstar <gyj5586@gmail.com>
2023-05-14 20:05:24 +0900

    Project setting

---
+++ src/main/java/com/genieday/bloome/BloomeApplication.java
@@ -0,0 +1,13 @@
+package com.genieday.bloome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genieday/bloome/BloomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genieday.bloome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
