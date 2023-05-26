c2c719f7448015e9d0aba6843748790ca184422a
yejinstar <gyj5586@gmail.com>
2023-05-26 22:33:05 +0900

    Feat: #4 로그인 기능 구현
    - RedisAccessTokenUtil 기존 key, value 값 설정 기준을 email -> idName으로 변경
    
    #4

--- src/main/java/com/genieday/bloome/security/RedisAccessTokenUtil.java
+++ src/main/java/com/genieday/bloome/security/RedisAccessTokenUtil.java
@@ -16,24 +16,24 @@ public class RedisAccessTokenUtil {
     private final int LIMIT_TIME = 60 * 60; // 1h
     private final StringRedisTemplate stringRedisTemplate;
 
-    public void saveAccessToken(String email, String jwt) {
-        stringRedisTemplate.opsForValue().set(PREFIX + email, jwt, Duration.ofSeconds(LIMIT_TIME));
+    public void saveAccessToken(String idName, String jwt) {
+        stringRedisTemplate.opsForValue().set(PREFIX + idName, jwt, Duration.ofSeconds(LIMIT_TIME));
     }
 
     public void saveBlockAccessToken(String jwt) {
         stringRedisTemplate.opsForValue().set(BLOCK_PREFIX + jwt, "block", Duration.ofSeconds(LIMIT_TIME));
     }
 
-    public String getAccessToken(String email) {
-        return stringRedisTemplate.opsForValue().get(PREFIX + email);
+    public String getAccessToken(String idName) {
+        return stringRedisTemplate.opsForValue().get(PREFIX + idName);
     }
 
-    public void deleteAccessToken(String email) {
-        stringRedisTemplate.delete(PREFIX + email);
+    public void deleteAccessToken(String idName) {
+        stringRedisTemplate.delete(PREFIX + idName);
     }
 
-    public boolean hasAccessToken(String email) {
-        return stringRedisTemplate.hasKey(PREFIX + email);
+    public boolean hasAccessToken(String idName) {
+        return stringRedisTemplate.hasKey(PREFIX + idName);
     }
 
     public boolean hasBlockAccessToken(String jwt) {
